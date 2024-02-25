74c68bbca3f2c1ffdc422d5fcba4150a1700cf06
IvanBuzin <ua.buzin@gmail.com>
2024-02-25 21:40:11 +0200

    fin

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,55 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
+import "../node_modules/modern-normalize/modern-normalize.css";
+import { Description } from "./components/Description/Description";
+import { Feedback } from "./components/Feedback/Feedback";
+import { Options } from "./components/Options/Options";
+import { Notification } from "./components/Notification/Notification";
 
-function App() {
-  const [count, setCount] = useState(0);
+const feedbackInfo = () => {
+  const feedbackValue = localStorage.getItem("feedbackInfo");
+  return feedbackValue !== null
+    ? JSON.parse(feedbackValue)
+    : {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      };
+};
+
+export const App = () => {
+  const [feedback, setFeedback] = useState(feedbackInfo);
+  const updateFeedback = (feedbackType) => {
+    setFeedback({
+      ...feedback,
+      [feedbackType]: feedback[feedbackType] + 1,
+    });
+  };
+
+  const totalFedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveInfo = Math.round(
+    ((feedback.good + feedback.neutral) / totalFedback) * 100
+  );
+
+  useEffect(() => {
+    localStorage.setItem("feedbackInfo", JSON.stringify(feedback));
+  }, [feedback]);
 
   return (
-    <>
-      <div>
-        <h1>Sip Happens Café</h1>
-        <p>
-          Please leave your feedback about our service by selecting one of the
-          options below.
-        </p>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Description />
+      <Options
+        onTrack={(feedbackType) => updateFeedback(feedbackType)}
+        totalFeed={totalFedback}
+        setFeedback={setFeedback}
+      />
+      {totalFedback >= 1 && (
+        <Feedback
+          value={feedback}
+          totalFeed={totalFedback}
+          info={positiveInfo}
+        />
+      )}
+      {totalFedback < 1 && <Notification />}
+    </div>
   );
-}
-
-export default App;
+};
